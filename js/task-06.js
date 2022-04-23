08fa1f0e1a2e9526cd45c9098e1e3725e5d2d766
Igor Ostapenko <igorostap9@gmail.com>
2022-04-23 21:41:06 +0300

    2

--- js/task-06.js
+++ js/task-06.js
@@ -3,9 +3,13 @@ const inputText = document.querySelector('[id="validation-input"]');
 inputText.addEventListener('blur', (event) => {
     
     let number = event.currentTarget.value.length;
-    if (number !== 6 ) {
-        inputText.classList.add('invalid');
-    } else {
+    if (number == inputText.dataset.length ) {
         inputText.classList.add('valid');
+        console.log(inputText.dataset.length);
+        if (inputText) {
+            inputText.classList.remove('invalid');
+        }
+    } else {
+        inputText.classList.add('invalid');
     }
 });
